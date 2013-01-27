c1ff789bc62fbcb8004c317ea6263b0764adb29a
Hayden Faulds <fauldsh@gmail.com>
2013-01-27 15:28:27 +0000

    player input

--- src/main/java/controllers/PlayerInputController.java
+++ src/main/java/controllers/PlayerInputController.java
@@ -1,11 +1,32 @@
 package controllers;
+
+import com.jogamp.newt.event.KeyEvent;
+
 import actors.Actor;
 
-
 public class PlayerInputController {
+  
+  private static final int KEY_FORWARD = 'W';
+
+  private Actor actor;
+  
+  private double accelMag = 0;
 
   public void possess(Actor actor) {
-    
+    this.actor = actor;
+  }
+  
+  public void tick(long dt) {
+    actor.setAcceleration(accelMag);
+  }
+
+
+  public void keyPressed(KeyEvent e) {
+    switch(e.getKeyCode()) {
+      case KEY_FORWARD:
+        accelMag = 1;
+        break;
+    }
   }
 
 }
--- src/test/java/controllers/PlayerInputControllerTest.java
+++ src/test/java/controllers/PlayerInputControllerTest.java
@@ -1,14 +1,38 @@
 package controllers;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import actors.Actor;
+
+import com.jogamp.newt.event.KeyEvent;
+
 public class PlayerInputControllerTest {
+  
+  private PlayerInputController input;
+  private Actor actor = mock(Actor.class);
+  
+  @Before
+  public void setup() {
+    input = new PlayerInputController();
+    
+    input.possess(actor);
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void forwardInputAcceleratesPlayer() {
+    KeyEvent keyEvent = mock(KeyEvent.class);
+    
+    when(keyEvent.getKeyCode()).thenReturn((int) 'W');
+    
+    input.keyPressed(keyEvent);
+    input.tick(10);
+    
+    verify(actor).setAcceleration(1);
   }
 
 }
