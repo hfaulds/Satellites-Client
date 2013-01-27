6e74f11c42d340aa268f3bbecb55e4237f526f52
Hayden Faulds <fauldsh@gmail.com>
2013-01-27 01:21:25 +0000

    added information to login msgs so i could add equals

--- src/main/java/net/msgs/LoginErrorMsg.java
+++ src/main/java/net/msgs/LoginErrorMsg.java
@@ -1,5 +1,36 @@
 package net.msgs;
 
 public class LoginErrorMsg {
+  
+  private final String error;
+
+  public LoginErrorMsg() {
+    this("");
+  }
+
+  public LoginErrorMsg(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof LoginErrorMsg))
+      return false;
+    LoginErrorMsg other = (LoginErrorMsg) obj;
+    if (error == null) {
+      if (other.error != null)
+        return false;
+    } else if (!error.equals(other.error))
+      return false;
+    return true;
+  }
 
 }
--- src/main/java/net/msgs/LoginRequestMsg.java
+++ src/main/java/net/msgs/LoginRequestMsg.java
@@ -2,4 +2,42 @@ package net.msgs;
 
 public class LoginRequestMsg {
 
+  private final String username;
+  private final String password;
+
+  public LoginRequestMsg(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof LoginRequestMsg))
+      return false;
+    LoginRequestMsg other = (LoginRequestMsg) obj;
+    if (password == null) {
+      if (other.password != null)
+        return false;
+    } else if (!password.equals(other.password))
+      return false;
+    if (username == null) {
+      if (other.username != null)
+        return false;
+    } else if (!username.equals(other.username))
+      return false;
+    return true;
+  }
+
 }
