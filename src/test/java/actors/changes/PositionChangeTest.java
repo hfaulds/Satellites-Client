5aaf7f17d2dc10eb94a1d692425d715d59d72d44
Hayden Faulds <fauldsh@gmail.com>
2013-01-27 01:45:27 +0000

    position change test

---
+++ src/test/java/actors/changes/PositionChangeTest.java
@@ -0,0 +1,29 @@
+package actors.changes;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import game.Vector2D;
+
+import org.junit.Test;
+
+import actors.Actor;
+
+public class PositionChangeTest {
+  
+  @Test
+  public void applyTo() {
+    Vector2D newPosition = mock(Vector2D.class);
+    PositionChange change = new PositionChange(newPosition);
+    
+    Actor actor = mock(Actor.class);
+    Vector2D oldPosition = mock(Vector2D.class);
+    
+    when(actor.getPosition()).thenReturn(oldPosition);
+    
+    change.applyTo(actor);
+    
+    verify(oldPosition)._set(newPosition);
+  }
+
+}
